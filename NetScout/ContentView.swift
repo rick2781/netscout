36b36e97d9cd259c9339745e044f389de0015dd6
rickpms <ricksofteng@gmail.com>
2019-06-19 21:16:48 -0600

    Setup Project

---
+++ NetScout/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  NetScout
+//
+//  Created by Rick Prata on 6/19/19.
+//  Copyright © 2019 NetScout. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
