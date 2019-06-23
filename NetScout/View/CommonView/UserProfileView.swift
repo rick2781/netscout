962a8720064ddb271b679a94bd41fec90b3d9597
rickpms <ricksofteng@gmail.com>
2019-06-23 00:07:00 -0600

    Implemented UserProfileView

--- NetScout/View/CommonView/UserProfileView.swift
+++ NetScout/View/CommonView/UserProfileView.swift
@@ -14,14 +14,35 @@ struct UserProfileView : View {
     
     var body: some View {
         
-        ForEach()
+        HStack() {
+            
+            ForEach(userProfileList.identified(by: \.self)) { user in
+                
+                // Wait until this bug gets fixed - Ambiguous reference to member
+//                if userProfileList.firstIndex(of: user) != 4 {
+                    
+                    URLImage(URL(string: user.profilePic!)!)
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .aspectRatio(CGSize(width: 30, height: 30), contentMode: .fit)
+                        .clipShape(Circle())
+                        .overlay(
+                            Circle().stroke(Color.white, lineWidth: 2))
+                        .padding(-6)
+                    
+//                } else {
+//
+//                    Text(userProfileList.count)
+//                }
+            }
+        }
     }
 }
 
 #if DEBUG
 struct UserProfileView_Previews : PreviewProvider {
     static var previews: some View {
-        UserProfileView()
+        UserProfileView(userProfileList: [UserModel(id: UUID().uuidString, displayName: "Rick Prata", email: "testrick@gmail.com", phoneNumber: "1254845612", profilePic: "https://www.w3schools.com/howto/img_snow.jpg"), UserModel(id: UUID().uuidString, displayName: "Rick Prata", email: "testrick@gmail.com", phoneNumber: "1254845612", profilePic: "https://www.w3schools.com/howto/img_snow.jpg"),UserModel(id: UUID().uuidString, displayName: "Rick Prata", email: "testrick@gmail.com", phoneNumber: "1254845612", profilePic: "https://www.w3schools.com/howto/img_snow.jpg"),UserModel(id: UUID().uuidString, displayName: "Rick Prata", email: "testrick@gmail.com", phoneNumber: "1254845612", profilePic: "https://www.w3schools.com/howto/img_snow.jpg")])
     }
 }
 #endif
